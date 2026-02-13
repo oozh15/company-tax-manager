dcb52109e80db5061fa6d6d68ff02cad601e09e3
YOGESH P <oozh150804@gmail.com>
2026-02-14 01:23:11 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+// Database Parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'company_finance');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root (Dynamic link for your project)
+define('URLROOT', 'http://localhost/tax-manager');
+
+// Site Name
+define('SITENAME', 'CorpTax Pro');
+
+// App Version
+define('APPVERSION', '1.0.0');
